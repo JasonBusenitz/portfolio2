e7c3f49585e9e30bdd1646984b01085540455b12
Jason Busenitz <jbus731@gmail.com>
2025-01-01 15:01:14 -0600

    About me info

---
+++ src/components/intro.jsx
@@ -0,0 +1,20 @@
+export default function Intro() {
+
+    return (
+        <>
+            <section>
+                <img src="" alt="Handsome man with a white dress shirt" />
+                <h1>
+                    Jason Busenitz
+                </h1>
+                <h2>
+                    Front End Developer (React)
+                </h2>
+                <h3>
+                    &#127759; Tecumseh, KS
+                </h3>
+            </section>
+
+        </>
+    )
+}
