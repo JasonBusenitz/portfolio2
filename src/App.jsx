b95e9b401d7ee9f1f54b1f9ef5f693e2a8fa9bf9
Jason Busenitz <jbus731@gmail.com>
2025-01-14 22:05:17 -0600

    Added css reset sheet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Intro from "./components/intro";
 import Nav from "./components/externalNav";
 import Projects from "./components/projects";
 import Skills from "./components/skills";
+import './assets/css/app.css'
 
 
 function App() {
